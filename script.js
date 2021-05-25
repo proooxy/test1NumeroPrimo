799ccf366f62cb048444f84f7e6ed92105518b43
proooxy <81929172+proooxy@users.noreply.github.com>
2021-05-25 20:34:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ for (let i = 1; i <= num; i++){
 if(divisoes == 2){
     console.log(`${num} É um numero primo`);
 }else{
-    console.log(`${num} Não é um numero primo`);
+    console.log(`${num} Não é um numero primo`)
 }
